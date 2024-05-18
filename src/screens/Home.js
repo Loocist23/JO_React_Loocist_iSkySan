04b9eada5674e9e9de38979fef62015530b0bbbb
Anthony Zegnal <anthony@mirror404.com>
2024-05-18 21:33:50 +0200

    HAHAHA

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Bienvenue sur Notre Site</h1>
-            <p>Ceci est la page d'accueil de notre merveilleux site. Ici, vous trouverez des informations sur nos sports favoris, les derniers résultats, et plus encore!</p>
+            <h1>Bienvenue sur Notre Site des Jeux Olympiques 2024</h1>
+            <p>Ceci est la page d'accueil de notre merveilleux site. Ici, vous trouverez des informations sur les Jeux Olympiques 2024.</p>
+            <h2>Les Sports</h2>
+            <p>Liste des sports...</p>
+            <h2>Nombre de Participants</h2>
+            <p>Nombre de participants...</p>
+            <h2>Autres Informations</h2>
+            <p>Autres informations...</p>
         </div>
     );
 };
